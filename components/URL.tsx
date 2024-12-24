d63487638edb4d3dec641835ce10195b80e9d1e0
Sankalp <sanklpkaushik21@gmail.com>
2024-12-24 11:14:17 +0530

    fixing url

--- components/URL.tsx
+++ components/URL.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 import { FaClipboard, FaClipboardCheck } from "react-icons/fa";
 
 const UrlComponent = () => {
